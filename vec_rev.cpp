0a55526b9baa9928e9c224aca721c13eda65cbf1
Md Yusus Siyam <mdsiyam1011@gmail.com>
2025-08-21 21:41:08 +0600

    Vector reverse

---
+++ vec_rev.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={2,5,8,4,0,3};
+    reverse(v.begin(),v.end());
+
+    for (auto u :v){
+        cout << u <<" " ;
+
+    }
+}
