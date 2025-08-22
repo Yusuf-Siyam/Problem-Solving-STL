53b13526ff1d86701f19f89bc9f558f8b5316277
Md Yusus Siyam <mdsiyam1011@gmail.com>
2025-08-22 11:37:11 +0600

    min max value and 2D vector

--- vec_min_max.cpp
+++ vec_min_max.cpp
@@ -1,15 +1,16 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     vector<int> v={ 4,3,6,8,1,6,3,5,9};
+ /*
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={ 4,3,6,8,1,6,3,5,9};
     
-    // vector<int>::iterator it = max_element(v.begin(), v.end()); //maximum
+    vector<int>::iterator it = max_element(v.begin(), v.end()); //maximum
 
-//     vector<int>::iterator it = max_element(v.begin()+1, v.begin()+4);
+    vector<int>::iterator it = max_element(v.begin()+1, v.begin()+4);
 
 
-//     cout << " max num is " << *it <<endl;
-// }
+    cout << " max num is " << *it <<endl;
+}
 
      //int max=*max_element(v.begin(),v.end());
      //max_element() return kore iterator Points to the max value
@@ -24,6 +25,30 @@ int main(){
     //vector<int>::iterator it = min_element(v.begin()+1, v.begin()+4);
 
 
-    cout << " max num is " << *it <<endl;
+    cout << " min num is " << *it <<endl;
 }
+*/
 //complexity O(n)
+//--------------2D vector-----------
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector <vector<int> > v;
+    vector<int> a1= { 3,43,4,3};
+    vector<int> a2= { 3,43,4,3};
+    v.push_back(a1);
+    v.push_back(a2);
+
+    for(auto u:v){
+        for (auto g:u){
+            cout << g << " ";
+            cout<< endl;
+
+        }
+    }
+    
+    
+
+}
