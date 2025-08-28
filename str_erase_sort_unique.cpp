1206369cb9415d6969137a70b074f7e1f6168bf3
Md Yusus Siyam <mdsiyam1011@gmail.com>
2025-08-28 22:43:11 +0600

    erease spacific, sort and unique string

--- str_erase_sort_unique.cpp
+++ str_erase_sort_unique.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 int main()
 {
-//     string s="SIYAM";
-//     s.erase(s.begin()+1); remove I
-//     s.erase(s.end()-1); remove A
-//     cout << s <<endl;
-// 
-   vector<string> v;
-   v.push_back("Siyam");
-   v.push_back("yusuf");
-   v.push_back("Rakib");
-   v.push_back("siyam");
+ 
+   // vector<string> v;
+   // v.push_back("Siyam");
+   // v.push_back("yusuf");
+   // v.push_back("Rakib");
+   // v.push_back("siyam");
 
-//   sort (v.begin(),v.end()); // lower case to upper case
-//   sort (v.rbegin(),v.rend()); // lower case to upper case
+
+//unique string ki ki 
+// sort(v.begin(),v.end());
+// int sz=unique(v.begin(),v.end())-v.begin();
+
+// cout<< sz << endl;
+
+// for(int i=0;i<sz;i++) cout<< v[i] << endl;
+
+//-------------------------------------
+//   sort (v.begin(),v.end()); // lower case to upper case sort 
+//   sort (v.rbegin(),v.rend()); // upper case to lower case sort 
 //    for ( auto u : v)
 //    cout << u <<endl;
 
-//unique string 
-sort(v.begin(),v.end());
-int sz=unique(v.begin(),v.end())-v.begin();
-
-cout<< sz << endl;
-
-for(int i=0;i<sz;i++) cout<< v[i] << endl;
+//-----remove spacific cheracter----
+string s="siyaaaaam";
+s.erase(remove(s.begin(),s.end(),'a'),s.end());
+cout << s<< endl;
 
 
 }
